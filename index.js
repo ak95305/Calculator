e5e390113b9021fc7ed664a1c0b88065c1c865e4
Aman Kumar <72601463+ak95305@users.noreply.github.com>
2021-01-08 18:38:32 +0530

    Calculator

---
+++ index.js
@@ -0,0 +1,70 @@
+let numbers = document.querySelectorAll(".number");
+let oprator = document.querySelectorAll(".oprator");
+let live = document.querySelector("#live");
+let previous = document.querySelector("#previous");
+let ac = document.querySelector(".ac");
+let c = document.querySelector(".c");
+let sign = document.querySelector("#sign");
+let equal = document.querySelector(".equal");
+
+let symbol;
+
+let operation;
+
+//numbers
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", numberFunction);
+
+  function numberFunction() {
+    live.innerHTML += numbers[i].textContent;
+  }
+}
+
+//oprator
+for (let i = 0; i < oprator.length; i++) {
+  oprator[i].addEventListener("click", opratorFunction);
+
+  function opratorFunction() {
+    if (previous.innerHTML === "") {
+      previous.innerHTML = live.innerHTML;
+    } else if(sign.innerHTML == ""){
+        previous.innerHTML = live.innerHTML;
+    } else {
+        previous.innerHTML = previous.innerHTML + symbol + live.innerHTML;
+    }
+    live.innerHTML = "";
+    sign.innerHTML = oprator[i].textContent;
+    symbol = oprator[i].textContent;
+  }
+}
+
+//ALL CLEAR
+ac.addEventListener("click", acFunction);
+
+function acFunction() {
+  previous.innerHTML = "";
+  live.innerHTML = "";
+  sign.innerHTML = "";
+}
+
+//CLEAR
+c.addEventListener("click", clearFunction);
+
+function clearFunction() {
+  liveNumber = live.innerHTML.split("");
+  liveNumber.pop();
+  live.innerHTML = liveNumber.join("");
+}
+
+//EQUAL
+
+equal.addEventListener("click", equalFunction);
+
+function equalFunction() {
+  previous.innerHTML += sign.innerHTML;
+  previous.innerHTML += live.innerHTML;
+  sign.innerHTML = "";
+  
+  live.innerHTML = (eval(previous.innerHTML));
+  
+}
